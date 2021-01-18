8d0e281c1e24edaf7bf469ddb82f1bc3aa3caf4d
Unknown <unknown@example.com>
2021-01-18 16:42:54 +0100

    es1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Creare un oggetto palla che abbia le seguenti proprietà.
+// Nome = palla
+// Peso = 10
+// Attraverso un prompt dare la possibilità all'utente di modificare il peso della palla.
+
+var palla = {
+  nome : 'palla',
+  peso : 10
+};
+
+palla.peso = parseInt(prompt('Inserisci il peso'));
+
+console.log(palla);
